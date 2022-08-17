bfbf0e87e8f139a808ddc3c5119850e1468b03d2
unknown <liubomyr.lypetskyi@gmail.com>
2022-08-17 22:50:58 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const hourEl = document.getElementById('hour');
+const minutesEl = document.getElementById('minutes');
+const secondsEl = document.getElementById('seconds');
+const ampmEl = document.getElementById('ampm');
+
+function updateClock() {
+  let hour = new Date().getHours();
+  let minutes = new Date().getMinutes();
+  let seconds = new Date().getSeconds();
+  let amPm = 'AM';
+
+  if (hour > 12) {
+    hour = hour - 12;
+    amPm = 'PM';
+  }
+
+  hour = hour < 10 ? '0' + hour : hour;
+  minutes = minutes < 10 ? '0' + minutes : minutes;
+  seconds = seconds < 10 ? '0' + seconds : seconds;
+
+  hourEl.innerText = hour;
+  minutesEl.innerText = minutes;
+  secondsEl.innerText = seconds;
+  ampmEl.innerText = amPm;
+  setTimeout(() => {
+    updateClock();
+  }, 1000);
+}
+
+updateClock();
